d386623f8f0e7f1c2ef639a7a604ab13c9d1f3e7
Siyoon1004 <brunoj101004@gmail.com>
2026-08-06 20:43:16 -0600

    double-digit scores working for both sides!

--- src/main/java/frc/robot/pong/PongGame.java
+++ src/main/java/frc/robot/pong/PongGame.java
@@ -21,6 +21,13 @@ public class PongGame {
     public static boolean lBotRShow;
     public static boolean lTopLShow;
     public static boolean lBotLShow;
+    public static boolean lTTopShow;
+    public static boolean lTMidShow;
+    public static boolean lTBotShow;
+    public static boolean lTTopRShow;
+    public static boolean lTBotRShow;
+    public static boolean lTTopLShow;
+    public static boolean lTBotLShow;
 
     public static boolean rTopShow;
     public static boolean rMidShow;
@@ -29,9 +36,18 @@ public class PongGame {
     public static boolean rBotRShow;
     public static boolean rTopLShow;
     public static boolean rBotLShow;
+    public static boolean rTTopShow;
+    public static boolean rTMidShow;
+    public static boolean rTBotShow;
+    public static boolean rTTopRShow;
+    public static boolean rTBotRShow;
+    public static boolean rTTopLShow;
+    public static boolean rTBotLShow;
 
     public int leftScore;
+    public int leftTens = leftScore - leftScore % 10;
     public int rightScore;
+    public int rightOnes = rightScore - rightScore % 10;
 
     /* 
      * TODO #6
@@ -93,7 +109,7 @@ public class PongGame {
         
         }
 
-        if (leftScore == 0) {
+        if (leftScore % 10 == 0) {
             lTopShow = true;
             lMidShow = false;    
             lBotShow = true;
@@ -101,7 +117,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = true;
             lBotLShow = true;
-        } else if(leftScore == 1) {
+        } else if(leftScore % 10 == 1) {
             lTopShow = false;
             lMidShow = false;    
             lBotShow = false;
@@ -109,7 +125,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = false;
             lBotLShow = false;
-        } else if (leftScore == 2) {
+        } else if (leftScore % 10 == 2) {
             lTopShow = true;
             lMidShow = true;    
             lBotShow = true;
@@ -117,7 +133,7 @@ public class PongGame {
             lBotRShow = false;
             lTopLShow = false;
             lBotLShow = true;
-        } else if (leftScore == 3) {
+        } else if (leftScore % 10 == 3) {
             lTopShow = true;
             lMidShow = true;    
             lBotShow = true;
@@ -125,7 +141,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = false;
             lBotLShow = false;
-        } else if (leftScore == 4) {
+        } else if (leftScore % 10 == 4) {
             lTopShow = false;
             lMidShow = true;    
             lBotShow = false;
@@ -133,7 +149,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = true;
             lBotLShow = false;
-        } else if (leftScore == 5) {
+        } else if (leftScore % 10 == 5) {
             lTopShow = true;
             lMidShow = true;    
             lBotShow = true;
@@ -141,7 +157,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = true;
             lBotLShow = false;
-        } else if (leftScore == 6) {
+        } else if (leftScore % 10 == 6) {
             lTopShow = true;
             lMidShow = true;    
             lBotShow = true;
@@ -149,7 +165,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = true;
             lBotLShow = true;
-        } else if (leftScore == 7) {
+        } else if (leftScore % 10 == 7) {
             lTopShow = true;
             lMidShow = false;    
             lBotShow = false;
@@ -157,7 +173,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = false;
             lBotLShow = false;
-        } else if (leftScore == 8) {
+        } else if (leftScore % 10 == 8) {
             lTopShow = true;
             lMidShow = true;    
             lBotShow = true;
@@ -165,7 +181,7 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = true;
             lBotLShow = true;
-        } else if (leftScore == 9) {
+        } else if (leftScore % 10 == 9) {
             lTopShow = true;
             lMidShow = true;    
             lBotShow = true;
@@ -173,13 +189,93 @@ public class PongGame {
             lBotRShow = true;
             lTopLShow = true;
             lBotLShow = false;
-        } else {
-            System.out.println("Left wins");
+            
+        } 
+        //__________________________-
+        if (leftScore - leftScore % 10 == 0) {
+            lTTopShow = true;
+            lTMidShow = false;    
+            lTBotShow = true;
+            lTTopRShow = true;
+            lTBotRShow = true;
+            lTTopLShow = true;
+            lTBotLShow = true;
+        } else if(leftScore - leftScore % 10 == 10) {
+            lTTopShow = false;
+            lTMidShow = false;    
+            lTBotShow = false;
+            lTTopRShow = true;
+            lTBotRShow = true;
+            lTTopLShow = false;
+            lTBotLShow = false;
+        } else if (leftScore - leftScore % 10 == 20) {
+            lTTopShow = true;
+            lTMidShow = true;    
+            lTBotShow = true;
+            lTTopRShow = true;
+            lTBotRShow = false;
+            lTTopLShow = false;
+            lTBotLShow = true;
+        } else if (leftScore - leftScore % 10 == 30) {
+            lTTopShow = true;
+            lTMidShow = true;    
+            lTBotShow = true;
+            lTTopRShow = true;
+            lTBotRShow = true;
+            lTTopLShow = false;
+            lTBotLShow = false;
+        } else if (leftScore - leftScore % 10 == 40) {
+            lTTopShow = false;
+            lTMidShow = true;    
+            lTBotShow = false;
+            lTTopRShow = true;
+            lTBotRShow = true;
+            lTTopLShow = true;
+            lTBotLShow = false;
+        } else if (leftScore - leftScore % 10 == 50) {
+            lTTopShow = true;
+            lTMidShow = true;    
+            lTBotShow = true;
+            lTTopRShow = false;
+            lTBotRShow = true;
+            lTTopLShow = true;
+            lTBotLShow = false;
+        } else if (leftScore - leftScore % 10 == 60) {
+            lTTopShow = true;
+            lTMidShow = true;    
+            lTBotShow = true;
+            lTTopRShow = false;
+            lTBotRShow = true;
+            lTTopLShow = true;
+            lTBotLShow = true;
+        } else if (leftScore - leftScore % 10 == 70) {
+            lTTopShow = true;
+            lTMidShow = false;    
+            lTBotShow = false;
+            lTTopRShow = true;
+            lTBotRShow = true;
+            lTTopLShow = false;
+            lTBotLShow = false;
+        } else if (leftScore - leftScore % 10 == 80) {
+            lTTopShow = true;
+            lTMidShow = true;    
+            lTBotShow = true;
+            lTTopRShow = true;
+            lTBotRShow = true;
+            lTTopLShow = true;
+            lTBotLShow = true;
+        } else if (leftScore - leftScore % 10 == 90) {
+            lTTopShow = true;
+            lTMidShow = true;    
+            lTBotShow = true;
+            lTTopRShow = true;
+            lTBotRShow = true;
+            lTTopLShow = true;
+            lTBotLShow = false;
         }
-
         //___________________________--
 
-        if (rightScore == 0) {
+        if (rightScore - rightScore % 10 == 0) {
             rTopShow = true;
             rMidShow = false;    
             rBotShow = true;
@@ -187,7 +283,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = true;
             rBotLShow = true;
-        } else if(rightScore == 1) {
+        } else if(rightScore - rightScore % 10 == 10) {
             rTopShow = false;
             rMidShow = false;    
             rBotShow = false;
@@ -195,7 +291,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = false;
             rBotLShow = false;
-        } else if (rightScore == 2) {
+        } else if (rightScore - rightScore % 10 == 20) {
             rTopShow = true;
             rMidShow = true;    
             rBotShow = true;
@@ -203,7 +299,7 @@ public class PongGame {
             rBotRShow = false;
             rTopLShow = false;
             rBotLShow = true;
-        } else if (rightScore == 3) {
+        } else if (rightScore - rightScore % 10 == 30) {
             rTopShow = true;
             rMidShow = true;    
             rBotShow = true;
@@ -211,7 +307,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = false;
             rBotLShow = false;
-        } else if (rightScore == 4) {
+        } else if (rightScore - rightScore % 10 == 40) {
             rTopShow = false;
             rMidShow = true;    
             rBotShow = false;
@@ -219,7 +315,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = true;
             rBotLShow = false;
-        } else if (rightScore == 5) {
+        } else if (rightScore - rightScore % 10 == 50) {
             rTopShow = true;
             rMidShow = true;    
             rBotShow = true;
@@ -227,7 +323,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = true;
             rBotLShow = false;
-        } else if (rightScore == 6) {
+        } else if (rightScore - rightScore % 10 == 60) {
             rTopShow = true;
             rMidShow = true;    
             rBotShow = true;
@@ -235,7 +331,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = true;
             rBotLShow = true;
-        } else if (rightScore == 7) {
+        } else if (rightScore - rightScore % 10 == 70) {
             rTopShow = true;
             rMidShow = false;    
             rBotShow = false;
@@ -243,7 +339,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = false;
             rBotLShow = false;
-        } else if (rightScore == 8) {
+        } else if (rightScore - rightScore % 10 == 80) {
             rTopShow = true;
             rMidShow = true;    
             rBotShow = true;
@@ -251,7 +347,7 @@ public class PongGame {
             rBotRShow = true;
             rTopLShow = true;
             rBotLShow = true;
-        } else if (rightScore == 9) {
+        } else if (rightScore - rightScore % 10 == 90) {
             rTopShow = true;
             rMidShow = true;    
             rBotShow = true;
@@ -260,7 +356,89 @@ public class PongGame {
             rTopLShow = true;
             rBotLShow = false;
         } else {
-            System.out.println("Right wins");
+            // System.out.println("Right wins");
+        }
+        //___________________________-
+        if (rightScore % 10 == 0) {
+            rTTopShow = true;
+            rTMidShow = false;    
+            rTBotShow = true;
+            rTTopRShow = true;
+            rTBotRShow = true;
+            rTTopLShow = true;
+            rTBotLShow = true;
+        } else if(rightScore % 10 == 1) {
+            rTTopShow = false;
+            rTMidShow = false;    
+            rTBotShow = false;
+            rTTopRShow = true;
+            rTBotRShow = true;
+            rTTopLShow = false;
+            rTBotLShow = false;
+        } else if (rightScore % 10 == 2) {
+            rTTopShow = true;
+            rTMidShow = true;    
+            rTBotShow = true;
+            rTTopRShow = true;
+            rTBotRShow = false;
+            rTTopLShow = false;
+            rTBotLShow = true;
+        } else if (rightScore % 10 == 3) {
+            rTTopShow = true;
+            rTMidShow = true;    
+            rTBotShow = true;
+            rTTopRShow = true;
+            rTBotRShow = true;
+            rTTopLShow = false;
+            rTBotLShow = false;
+        } else if (rightScore % 10 == 4) {
+            rTTopShow = false;
+            rTMidShow = true;    
+            rTBotShow = false;
+            rTTopRShow = true;
+            rTBotRShow = true;
+            rTTopLShow = true;
+            rTBotLShow = false;
+        } else if (rightScore % 10 == 5) {
+            rTTopShow = true;
+            rTMidShow = true;    
+            rTBotShow = true;
+            rTTopRShow = false;
+            rTBotRShow = true;
+            rTTopLShow = true;
+            rTBotLShow = false;
+        } else if (rightScore % 10 == 6) {
+            rTTopShow = true;
+            rTMidShow = true;    
+            rTBotShow = true;
+            rTTopRShow = false;
+            rTBotRShow = true;
+            rTTopLShow = true;
+            rTBotLShow = true;
+        } else if (rightScore % 10 == 7) {
+            rTTopShow = true;
+            rTMidShow = false;    
+            rTBotShow = false;
+            rTTopRShow = true;
+            rTBotRShow = true;
+            rTTopLShow = false;
+            rTBotLShow = false;
+        } else if (rightScore % 10 == 8) {
+            rTTopShow = true;
+            rTMidShow = true;    
+            rTBotShow = true;
+            rTTopRShow = true;
+            rTBotRShow = true;
+            rTTopLShow = true;
+            rTBotLShow = true;
+        } else if (rightScore % 10 == 9) {
+            rTTopShow = true;
+            rTMidShow = true;    
+            rTBotShow = true;
+            rTTopRShow = true;
+            rTBotRShow = true;
+            rTTopLShow = true;
+            rTBotLShow = false;
         }
 
 
@@ -301,16 +479,32 @@ public class PongGame {
         else if (stickName == "lBotL") {
             return (lBotLShow) ? 49: 1000;
         } 
+        // ______---
+
+        if (stickName == "lTTop") {
+            return (lTTopShow) ? 43 : 1000;
+        }
+        else if(stickName == "lTMid") {
+            return (lTMidShow) ? 43: 1000;
+        }
+        else if (stickName == "lTBot") {
+            return (lTBotShow) ? 43: 1000;
+        }
+        else if (stickName == "lTTopR") {
+            return (lTTopRShow) ? 47: 1000;
+        }
+        else if (stickName == "lTBotR") {
+            return (lTBotRShow) ? 47: 1000;
+        }
+        else if (stickName == "lTTopL") {
+            return (lTTopLShow) ? 42: 1000;
+        }
+        else if (stickName == "lTBotL") {
+            return (lTBotLShow) ? 42: 1000;
+        } 
         //____
-        //fix X coords
-    //     private final MechanismRoot2d rTopRoot = court.getRoot("rTopRoot", 70, 50);
-    // private final MechanismRoot2d rMidRoot = court.getRoot("rMidRoot", 70, 45);
-    // private final MechanismRoot2d rBotRoot = court.getRoot("rBotRoot", 70, 40);
-    // private final MechanismRoot2d rTopRRoot = court.getRoot("rTopRRoot", 74, 46);
-    // private final MechanismRoot2d rBotRRoot = court.getRoot("rBotRRoot", 74, 41);
-    // private final MechanismRoot2d rTopLRoot = court.getRoot("rTopLRoot", 69, 46);
-    // private final MechanismRoot2d rBotLRoot = court.getRoot("rBotLRoot", 69, 41);
-        else if (stickName == "rTop") {
+       
+        if (stickName == "rTop") {
             return (rTopShow) ? 70: 1000;
         } 
         else if (stickName == "rMid") {
@@ -331,8 +525,30 @@ public class PongGame {
         else if (stickName == "rBotL") {
             return (rBotLShow) ? 69: 1000;
         } 
+        //______________-
+        if (stickName == "rTTop") {
+            return (rTTopShow) ? 77: 1000;
+        } 
+        else if (stickName == "rTMid") {
+            return (rTMidShow) ? 77: 1000;
+        } 
+        else if (stickName == "rTBot") {
+            return (rTBotShow) ? 77: 1000;
+        } 
+        else if (stickName == "rTTopR") {
+            return (rTTopRShow) ? 81: 1000;
+        } 
+        else if (stickName == "rTBotR") {
+            return (rTBotRShow) ? 81: 1000;
+        } 
+        else if (stickName == "rTTopL") {
+            return (rTTopLShow) ? 76: 1000;
+        } 
+        else if (stickName == "rTBotL") {
+            return (rTBotLShow) ? 76: 1000;
+        } 
         else {
-            System.out.println("Not working");
+            // System.out.println("Not working");
             return 0;
         }
         
--- src/main/java/frc/robot/pong/PongVisualizer.java
+++ src/main/java/frc/robot/pong/PongVisualizer.java
@@ -21,7 +21,7 @@ public class PongVisualizer {
     private final MechanismRoot2d lTopLRoot = court.getRoot("lTopLRoot", 49, 46);
     private final MechanismRoot2d lBotLRoot = court.getRoot("lBotLRoot", 49, 41);
 
-    //right side score
+    //right side tens score
     private final MechanismRoot2d rTopRoot = court.getRoot("rTopRoot", 70, 50);
     private final MechanismRoot2d rMidRoot = court.getRoot("rMidRoot", 70, 45);
     private final MechanismRoot2d rBotRoot = court.getRoot("rBotRoot", 70, 40);
@@ -29,6 +29,24 @@ public class PongVisualizer {
     private final MechanismRoot2d rBotRRoot = court.getRoot("rBotRRoot", 74, 41);
     private final MechanismRoot2d rTopLRoot = court.getRoot("rTopLRoot", 69, 46);
     private final MechanismRoot2d rBotLRoot = court.getRoot("rBotLRoot", 69, 41);
+
+    //left side tens 
+    private final MechanismRoot2d lTTopRoot = court.getRoot("lTTopRoot", 44, 50);
+    private final MechanismRoot2d lTMidRoot = court.getRoot("lTMidRoot", 44, 45);
+    private final MechanismRoot2d lTBotRoot = court.getRoot("lTBotRoot", 44, 40);
+    private final MechanismRoot2d lTTopRRoot = court.getRoot("lTTopRRoot", 48, 46);
+    private final MechanismRoot2d lTBotRRoot = court.getRoot("lTBotRRoot", 48, 41);
+    private final MechanismRoot2d lTTopLRoot = court.getRoot("lTTopLRoot", 43, 46);
+    private final MechanismRoot2d lTBotLRoot = court.getRoot("lTBotLRoot", 43, 41);
+
+    //right side ones
+    private final MechanismRoot2d rTTopRoot = court.getRoot("rTTopRoot", 76, 50);
+    private final MechanismRoot2d rTMidRoot = court.getRoot("rTMidRoot", 76, 45);
+    private final MechanismRoot2d rTBotRoot = court.getRoot("rTBotRoot", 76, 40);
+    private final MechanismRoot2d rTTopRRoot = court.getRoot("rTTopRRoot", 80, 46);
+    private final MechanismRoot2d rTBotRRoot = court.getRoot("rTBotRRoot", 80, 41);
+    private final MechanismRoot2d rTTopLRoot = court.getRoot("rTTopLRoot", 75, 46);
+    private final MechanismRoot2d rTBotLRoot = court.getRoot("rTBotLRoot", 75, 41);
 
 
 
@@ -43,15 +61,32 @@ public class PongVisualizer {
     private final MechanismLigament2d lBotR;
     private final MechanismLigament2d lTopL;
     private final MechanismLigament2d lBotL; 
+    //left tens
+    private final MechanismLigament2d lTTop;
+    private final MechanismLigament2d lTMid;
+    private final MechanismLigament2d lTBot;
+    private final MechanismLigament2d lTTopR;
+    private final MechanismLigament2d lTBotR;
+    private final MechanismLigament2d lTTopL;
+    private final MechanismLigament2d lTBotL; 
 
-    //right side
+    //right side ones
     private final MechanismLigament2d rTop;
     private final MechanismLigament2d rMid;
     private final MechanismLigament2d rBot;
     private final MechanismLigament2d rTopR;
     private final MechanismLigament2d rBotR;
     private final MechanismLigament2d rTopL;
-    private final MechanismLigament2d rBotL; 
+    private final MechanismLigament2d rBotL;
+
+    //right side tens
+    private final MechanismLigament2d rTTop;
+    private final MechanismLigament2d rTMid;
+    private final MechanismLigament2d rTBot;
+    private final MechanismLigament2d rTTopR;
+    private final MechanismLigament2d rTBotR;
+    private final MechanismLigament2d rTTopL;
+    private final MechanismLigament2d rTBotL; 
     
     public PongVisualizer() {
         this.leftPaddle = leftPaddleRoot.append(new MechanismLigament2d("leftPaddle", PongConstants.PADDLE_SIZE, 90));
@@ -66,6 +101,14 @@ public class PongVisualizer {
         this.lTopL = lTopLRoot. append(new MechanismLigament2d("lTopLRoot", 3, 90));
         this.lBotL = lBotLRoot. append(new MechanismLigament2d("lBotLRoot", 3, 90));
 
+        this.lTTop = lTTopRoot.append(new MechanismLigament2d("lTTop", 3, 0));
+        this.lTMid = lTMidRoot.append(new MechanismLigament2d("lTMid", 3, 0));
+        this.lTBot = lTBotRoot.append(new MechanismLigament2d("lTBot", 3, 0));
+        this.lTTopR = lTTopRRoot.append(new MechanismLigament2d("lTTopRRoot", 3, 90));
+        this.lTBotR = lTBotRRoot.append(new MechanismLigament2d("lTBotRRoot", 3, 90));
+        this.lTTopL = lTTopLRoot. append(new MechanismLigament2d("lTTopLRoot", 3, 90));
+        this.lTBotL = lTBotLRoot. append(new MechanismLigament2d("ltBotLRoot", 3, 90));
+
         this.rTop = rTopRoot.append(new MechanismLigament2d("rTop", 3, 0));
         this.rMid = rMidRoot.append(new MechanismLigament2d("rMid", 3, 0));
         this.rBot = rBotRoot.append(new MechanismLigament2d("rBot", 3, 0));
@@ -73,6 +116,14 @@ public class PongVisualizer {
         this.rBotR = rBotRRoot.append(new MechanismLigament2d("rBotRRoot", 3, 90));
         this.rTopL = rTopLRoot. append(new MechanismLigament2d("rTopLRoot", 3, 90));
         this.rBotL = rBotLRoot. append(new MechanismLigament2d("rBotLRoot", 3, 90));
+
+        this.rTTop = rTTopRoot.append(new MechanismLigament2d("rTTop", 3, 0));
+        this.rTMid = rTMidRoot.append(new MechanismLigament2d("rTMid", 3, 0));
+        this.rTBot = rTBotRoot.append(new MechanismLigament2d("rTBot", 3, 0));
+        this.rTTopR = rTTopRRoot.append(new MechanismLigament2d("rTTopRRoot", 3, 90));
+        this.rTBotR = rTBotRRoot.append(new MechanismLigament2d("rTBotRRoot", 3, 90));
+        this.rTTopL = rTTopLRoot. append(new MechanismLigament2d("rTTopLRoot", 3, 90));
+        this.rTBotL = rTBotLRoot. append(new MechanismLigament2d("rTBotLRoot", 3, 90));
         
         SmartDashboard.putData("PONG", court);       
     }
@@ -81,6 +132,7 @@ public class PongVisualizer {
         leftPaddleRoot.setPosition(PongConstants.LEFT_PADDLE_X, PongConstants.COURT_HEIGHT / 2 - PongConstants.PADDLE_SIZE / 2 + data.leftPaddlePosition());
         rightPaddleRoot.setPosition(PongConstants.RIGHT_PADDLE_X, PongConstants.COURT_HEIGHT / 2 - PongConstants.PADDLE_SIZE / 2 + data.rightPaddlePosition());
         ballRoot.setPosition(data.ballX(), data.ballY());
+
         lTopRoot.setPosition(PongGame.checkForScoreShow("lTop"), 50);
         lMidRoot.setPosition(PongGame.checkForScoreShow("lMid"), 45);
         lBotRoot.setPosition(PongGame.checkForScoreShow("lBot"), 40);
@@ -89,6 +141,14 @@ public class PongVisualizer {
         lTopLRoot.setPosition(PongGame.checkForScoreShow("lTopL"), 46);
         lBotLRoot.setPosition(PongGame.checkForScoreShow("lBotL"), 41);
 
+        lTTopRoot.setPosition(PongGame.checkForScoreShow("lTTop"), 50);
+        lTMidRoot.setPosition(PongGame.checkForScoreShow("lTMid"), 45);
+        lTBotRoot.setPosition(PongGame.checkForScoreShow("lTBot"), 40);
+        lTTopRRoot.setPosition(PongGame.checkForScoreShow("lTTopR"), 46);
+        lTBotRRoot.setPosition(PongGame.checkForScoreShow("lTBotR"), 41);
+        lTTopLRoot.setPosition(PongGame.checkForScoreShow("lTTopL"), 46);
+        lTBotLRoot.setPosition(PongGame.checkForScoreShow("lTBotL"), 41);
+
         rTopRoot.setPosition(PongGame.checkForScoreShow("rTop"), 50);
         rMidRoot.setPosition(PongGame.checkForScoreShow("rMid"), 45);
         rBotRoot.setPosition(PongGame.checkForScoreShow("rBot"), 40);
@@ -96,5 +156,13 @@ public class PongVisualizer {
         rBotRRoot.setPosition(PongGame.checkForScoreShow("rBotR"), 41);
         rTopLRoot.setPosition(PongGame.checkForScoreShow("rTopL"), 46);
         rBotLRoot.setPosition(PongGame.checkForScoreShow("rBotL"), 41);
+
+        rTTopRoot.setPosition(PongGame.checkForScoreShow("rTTop"), 50);
+        rTMidRoot.setPosition(PongGame.checkForScoreShow("rTMid"), 45);
+        rTBotRoot.setPosition(PongGame.checkForScoreShow("rTBot"), 40);
+        rTTopRRoot.setPosition(PongGame.checkForScoreShow("rTTopR"), 46);
+        rTBotRRoot.setPosition(PongGame.checkForScoreShow("rTBotR"), 41);
+        rTTopLRoot.setPosition(PongGame.checkForScoreShow("rTTopL"), 46);
+        rTBotLRoot.setPosition(PongGame.checkForScoreShow("rTBotL"), 41);
     }
 }
